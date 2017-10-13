6b55e2b75d23a9c67cdd665947524e1da79f751e
skygreen <skygreen2001@itasktour.com>
2017-10-13 18:21:57 +0800

    tweak

--- test.php
+++ test.php
@@ -227,3 +227,7 @@ require_once ("init.php");
 
 // LogMe::log("我在想事情呢！等等我");
 // LogMe::log("不急不急,休息一下！");
+
+if( contains( $_SERVER['HTTP_HOST'], array("127.0.0.1","localhost") ) ) {
+    phpinfo();
+}
