f37592c71a3546be004a468e0e1e64e4e5dac9af
skygreen2001 <skygreen2001@gmail.com>
2019-05-04 17:13:58 +0800

    with curl post and get

--- core/util/net/UtilNet.php
+++ core/util/net/UtilNet.php
@@ -215,6 +215,54 @@ class UtilNet extends Util
         //echo "你的IP:".$ip ;
         return $ip;
     }
+
+    /**
+     * curl https get
+     * @param string $url
+     * @return mixed
+     */
+    public static function curl_get($url) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+        curl_setopt($curl, CURLOPT_HEADER, 0); // 不要http header 加快效率
+        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+        $tmpInfo = curl_exec($curl);
+        if ( curl_errno($curl) ) {
+            echo '错误代码: ' . curl_error($curl);//捕抓异常
+        }
+        curl_close($curl); // 关闭CURL会话
+        return $tmpInfo; // 返回数据
+    }
+
+    /**
+     * curl https post
+     * @param string $url
+     * @param var $data
+     * @return mixed
+     */
+    public static function curl_post($url, $data) { // 模拟提交数据函数
+        $curl = curl_init(); // 启动一个CURL会话
+        curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
+        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+        curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data); // Post提交的数据包
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+        curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+
+        $tmpInfo = curl_exec($curl); // 执行操作
+        if ( curl_errno($curl) ) {
+            echo '错误代码: ' . curl_error($curl);//捕抓异常
+        }
+        curl_close($curl); // 关闭CURL会话
+        return $tmpInfo; // 返回数据
+    }
 }
 
 ?>
