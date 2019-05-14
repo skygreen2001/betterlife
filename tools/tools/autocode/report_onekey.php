1ff17906d7fd22f4031799224a565aa311497b7b
skygreen2001 <skygreen2001@gmail.com>
2019-05-14 17:36:52 +0800

    tweak

--- tools/tools/autocode/report_onekey.php
+++ tools/tools/autocode/report_onekey.php
@@ -26,15 +26,14 @@ if ($reportDev && !empty($reportCname) && !empty($reportSql)){
     );
     AutoCodeCreateReport::AutoCode( $config );
     $template_build_dev = <<<TPL_BUILDDEV
-        <card name='reportCard' style="width:600px;margin: -30px auto 50px auto;">
+        <card id="reportProdCard" name='reportProdCard' style="width:600px;margin: -30px auto 50px auto;">
             <h4 style="text-align: center">请去model目录下查看新创建的报表文件，确认无误后点击覆盖生成，生成正式文件</h4><br />
-            <i-form>
-                <input class="input_save_dir" type="hidden" name="report_cname" value="$reportCname" />
-                <input class="input_save_dir" type="hidden" name="report_ename" value="$reportEname" />
-                <input class="input_save_dir" type="hidden" name="report_desc" value="$reportDesc" />
-                <input class="input_save_dir" type="hidden" name="report_sql" value="$reportSql" />
-                <input type='hidden' name='report_prod' value='true'>
-                <i-button type='primary'>覆盖生成</i-button>
+            <i-form ref="reportProdForm" :model="reportProdForm">
+                <input type="hidden" v-model="reportProdForm.report_cname" name="report_cname" value="$reportCname" />
+                <input type="hidden" v-model="reportProdForm.report_ename" name="report_ename" value="$reportEname" />
+                <input type="hidden" v-model="reportProdForm.report_desc" name="report_desc" value="$reportDesc" />
+                <input type="hidden" v-model="reportProdForm.report_sql" name="report_sql" value="$reportSql" />
+                <i-button type='primary' @click="createReportProd">覆盖生成</i-button>
             </i-form>
         </card>
 TPL_BUILDDEV;
@@ -42,7 +41,7 @@ TPL_BUILDDEV;
 
 $reportProd = !empty($_REQUEST['report_prod']) ? $_REQUEST['report_prod'] : "";
 $template_build_prod = "";
-if ($reportProd && !empty($reportCname) && !empty($reportDesc) && !empty($reportSql)){
+if ($reportProd && !empty($reportCname) && !empty($reportSql)){
     $config = array(
         "isProd" => true,
         "reportType" => $reportType,
@@ -53,7 +52,7 @@ if ($reportProd && !empty($reportCname) && !empty($reportDesc) && !empty($report
     );
     AutoCodeCreateReport::AutoCode( $config );
     $template_build_prod = <<<TPL_BUILDPROD
-        <card name='reportCard' style="width:600px;margin: -30px auto 50px auto;">
+        <card id="reportInfoCard" name='reportInfoCard' style="width:600px;margin: -30px auto 50px auto;">
             <h4 style='text-align: center'>报表文件已生成至正式目录，请自行查看</h4>
         </card>
 TPL_BUILDPROD;
@@ -80,7 +79,7 @@ TPL_BUILDPROD;
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <div id="report-form" class="container" align="center">
-          <card name='reportCard' style="width:600px;margin: 150px auto 50px auto;">
+          <card id="reportCard" name='reportCard' style="width:600px;margin: 150px auto 50px auto;">
             <p slot="title" style="text-align:left;">
                 <i class="ivu-icon ivu-icon-ios-construct"></i> <?php echo $title ?>
             </p>
@@ -114,10 +113,6 @@ TPL_BUILDPROD;
                     <form-item label="报表所需SQL" prop="report_sql">
                       <i-input type="textarea" v-model="reportForm.report_sql" name="report_sql" :rows="10" :cols="58" :autosize="{minRows: 2,maxRows: 20}"  />
                     </form-item>
-                </div>
-
-                <div>
-                    <input type="hidden" v-model="reportForm.report_dev" name="report_dev" value="true" />
                 </div>
                 <i-button type="primary" @click="createReport">生成</i-button><br><br>
             </i-form>
@@ -156,6 +151,14 @@ TPL_BUILDPROD;
                 report_sql: "<?php echo $reportSql ?>",
                 report_dev: true
               },
+              reportProdForm: {
+                report_type: '<?php echo $reportType ?>',
+                report_cname: '<?php echo $reportCname ?>',
+                report_ename: '<?php echo $reportEname ?>',
+                report_desc: '<?php echo $reportDesc ?>',
+                report_sql: "<?php echo $reportSql ?>",
+                report_prod: true
+              },
               ruleValidate: {
                   report_cname: [
                       { required: true, message: '报表中文昵称输入不能为空', trigger: 'blur' }
@@ -176,7 +179,7 @@ TPL_BUILDPROD;
               createReport: function () {
                 this.$refs["reportForm"].validate((valid) => {
                     if (valid) {
-                        this.$Message.success('Success!');
+                        this.$Message.success('生成成功!');
                         var params = "";
                         for (var key in this.$data.reportForm) {
                             if (this.$data.reportForm[key]) {
@@ -186,12 +189,28 @@ TPL_BUILDPROD;
                               params += key + "=" + encodeURIComponent(this.$data.reportForm[key]);
                             }
                         }
-                        console.log("<?php echo $url_base;?>tools/tools/autocode/report_onekey.php?" + params);
+                        // console.log("<?php echo $url_base;?>tools/tools/autocode/report_onekey.php?" + params);
                         location.href = "<?php echo $url_base;?>tools/tools/autocode/report_onekey.php?" + params;
                     } else {
                         this.$Message.error('请按格式要求填写表格!');
                     }
-                })
+                });
+              },
+              createReportProd: function () {
+                if (this.$refs["reportProdForm"]) {
+                  var params = "";
+                  for (var key in this.$data.reportProdForm) {
+                      if (this.$data.reportProdForm[key]) {
+                          if (params != "") {
+                              params += "&";
+                          }
+                        params += key + "=" + encodeURIComponent(this.$data.reportProdForm[key]);
+                      }
+                  }
+                  // console.log("<?php echo $url_base;?>tools/tools/autocode/report_onekey.php?" + params);
+                  location.href = "<?php echo $url_base;?>tools/tools/autocode/report_onekey.php?" + params;
+
+                }
               },
               refresh: function() {
                 console.log("refresh");
