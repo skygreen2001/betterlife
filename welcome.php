8a0a7cf3cfa18415fad2b00decf5331612404d7a
skygreen2001 <skygreen2001@gmail.com>
2019-11-29 12:33:47 +0800

    optimize redis show and add config valid

--- welcome.php
+++ welcome.php
@@ -1,8 +1,8 @@
 <?php
-require_once ("Gc.php");
+require_once("Gc.php");
 require_once("core/include/common.php");
 if( !contains( $_SERVER['HTTP_HOST'], array("127.0.0.1", "localhost", "192.168.") ) ) {
-    header("location:".Gc::$url_base."index.php?go=".Gc::$appName.".index.index");
+    header("location:" . Gc::$url_base . "index.php?go=" . Gc::$appName . ".index.index");
     die();
 }
 ?>
@@ -213,29 +213,31 @@ if( !contains( $_SERVER['HTTP_HOST'], array("127.0.0.1", "localhost", "192.168."
     // 例子：
     // (new Date()).format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
     // (new Date()).format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
-    Date.prototype.format = function(fmt)
-    {
-      fmt = fmt || "yyyy-MM-dd";
-      var o = {
-        "M+" : this.getMonth()+1,                 //月份
-        "d+" : this.getDate(),                    //日
-        "h+" : this.getHours(),                   //小时
-        "m+" : this.getMinutes(),                 //分
-        "s+" : this.getSeconds(),                 //秒
-        "q+" : Math.floor((this.getMonth()+3)/3), //季度
-        "S"  : this.getMilliseconds()             //毫秒
-      };
-      if(/(y+)/.test(fmt))
-        fmt = fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
-      for(var k in o)
-        if(new RegExp("("+ k +")").test(fmt))
-          fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
-      return fmt;
+    Date.prototype.format = function(fmt) {
+        fmt = fmt || "yyyy-MM-dd";
+        var o = {
+          "M+" : this.getMonth()+1,                 //月份
+          "d+" : this.getDate(),                    //日
+          "h+" : this.getHours(),                   //小时
+          "m+" : this.getMinutes(),                 //分
+          "s+" : this.getSeconds(),                 //秒
+          "q+" : Math.floor((this.getMonth()+3)/3), //季度
+          "S"  : this.getMilliseconds()             //毫秒
+        };
+        if ( /(y+)/.test(fmt) ) {
+            fmt = fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+        }
+        for (var k in o) {
+            if ( new RegExp("("+ k +")").test(fmt) ) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : ( ("00" + o[k]).substr(("" + o[k]).length)) );
+            }
+        }
+        return fmt;
     };
     document.getElementById("current-time").innerHTML = new Date().format('yyyy-MM-dd hh:mm:ss');
-    setInterval(function(){
-      document.getElementById("current-time").innerHTML = new Date().format('yyyy-MM-dd hh:mm:ss');
-    },1000)
+    setInterval(function() {
+        document.getElementById("current-time").innerHTML = new Date().format('yyyy-MM-dd hh:mm:ss');
+    }, 1000)
     </script>
 </body>
 </html>
