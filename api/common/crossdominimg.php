f80c7dc8d8ef7b67a180fa75cee2562509c8a021
skygreen2001 <skygreen2001@gmail.com>
2021-12-30 16:37:12 +0800

    cross domain image show

--- api/common/crossdominimg.php
+++ api/common/crossdominimg.php
@@ -1,15 +1,32 @@
 <?php
-header("Content-Type: image/jpeg;");
+require_once ("../../init.php");
+
 $params  = $_GET;
 $img_src = $params['src'];
 
+if ( endWith( $img_src, ".mp4") ) {
+    header("Content-Type: video/mpeg4;");
+} else if ( endWith( $img_src, ".gif" ) ) {
+    header("Content-Type: image/gif;");
+} else if ( endWith( $img_src, ".png" ) ) {
+    header("Content-Type: image/png;");
+} else if ( endWith( $img_src, ".pdf" ) ) {
+    header("Content-Type: application/pdf;");
+} else if ( endWith( $img_src, ".pptx") ) {
+    header("Content-Type: application/vnd.openxmlformats-officedocument.presentationml.presentation");
+} else {
+    header("Content-Type: image/jpeg;");
+}
+
+// 方案一: file_get_contents
+// echo file_get_contents($img_src);
+
+// 方案二: curl
 // $img_src = 'https://images.nga.gov/en/web_images/constable.jpg'; //仅供测试
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $img_src);
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 // curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.1 Safari/537.11');
 $res = curl_exec($ch);
@@ -17,7 +34,7 @@ $res = curl_exec($ch);
 //Check for errors.
 if ( curl_errno($ch) ) {
     // [PHP - curl localhost connection refused](https://pretagteam.com/question/php-curl-localhost-connection-refused)
-    if ( strpos($img_src, "http://localhost/") !== false || strpos($img_src, "http://127.0.0.1/") !== false ) {
+    if ( contains( $img_src, array("127.0.0.1", "localhost", "192.168.", '.test') ) ) {
         curl_setopt($ch, CURLOPT_PROXY, $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT']); 
         $res = curl_exec($ch);
     }
